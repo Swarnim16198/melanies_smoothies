3bcc89f3679f5fad51dabed380c5886db9ae74c7
Swarnim16198 <sbhattad@arsa-infotech.com>
2025-04-08 15:08:18 +0200

    Update streamlit_app.py
    mopved smoothiefroot_response on the top and then removed the 2 lines as said in 3.10.3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Customize Your Smoothie ! :cup_with_straw:")
@@ -25,15 +26,13 @@ st.write(ingredients_list)
 st.text(ingredients_list)
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
-    
-
-ingredients_string = ''
+   ingredients_string = ''
 
 for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
-
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+    
 my_insert_stmt = """ insert into smoothies.public.order.ingredients)
 values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 #st.write(ingredients_string)
@@ -49,8 +48,3 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
 
 st.success('Your Smoothie is ordered!', icon="✅")
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
